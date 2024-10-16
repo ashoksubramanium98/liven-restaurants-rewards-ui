7b917e47b9b5ad297354616831202a694a51df5f
Padaipaaligal <padaipaaligal23@gmail.com>
2024-10-16 12:03:32 +0530

    #Add broken page for invalid routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { pages } from '../config' 
-import RewardsListing from '../pages/rewards-listing';
+import RewardsListing from '../pages/RewardsListing';
+import BrokenPage from '../pages/BrokenPage'
 
 const Routes = createBrowserRouter([
   {
     path: pages.rewardsListing,
-    element: <RewardsListing />
+    element: <RewardsListing />,
+    errorElement: <BrokenPage />,
   }
 ]);
 
